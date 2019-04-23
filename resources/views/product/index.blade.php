ca0a1a24957b5493df7c1b9b4bde5bc1d1177570
Alan2304 <sanchezalan95@gmail.com>
2019-04-23 16:21:52 -0500

    Add validation to check if the establishment has coupons

--- resources/views/product/index.blade.php
+++ resources/views/product/index.blade.php
@@ -16,21 +16,29 @@
             </div>
         @endif
         <div class="row">
-            @foreach ($products as $product)
-                <div class="col-md-4">
-                    <div class="card">
-                        <h5 class="card-header bg-dark text-white">{{$product->name}}</h5>
-                        <div class="card-body">
-                            <h5 class="card-title">Info</h5>
-                            <p class="card-text">Cost: {{$product->amount}}</p>
-                            <p class="card-text">Quantity: {{$product->quantity}}</p>
-                            <p class="card-text">Located At: {{$product->establishment->name}}</p>
-                            <a href="{{url('product/update/'.$product->id)}}" class="btn btn-info text-white">Modify</a>
-                            <a href="{{url('product/delete/'.$product->id)}}" class="btn btn-danger">Delete</a>
+            @if ($products->count() > 0)
+                @foreach ($products as $product)
+                    <div class="col-md-4">
+                        <div class="card">
+                            <h5 class="card-header bg-dark text-white">{{$product->name}}</h5>
+                            <div class="card-body">
+                                <h5 class="card-title">Info</h5>
+                                <p class="card-text">Cost: {{$product->amount}}</p>
+                                <p class="card-text">Quantity: {{$product->quantity}}</p>
+                                <p class="card-text">Located At: {{$product->establishment->name}}</p>
+                                <p class="card-text">Type: {{$product->type->name}}</p>
+                                <a href="{{url('product/update/'.$product->id)}}" class="btn btn-info text-white">Modify</a>
+                                <a href="{{url('product/delete/'.$product->id)}}" class="btn btn-danger">Delete</a>
+                                <a href="{{url('createCoupon/'.$product->id)}}" class="btn btn-success">Add Coupon</a>
+                            </div>
                         </div>
                     </div>
+                @endforeach
+            @else
+                <div class="col-md-12">
+                    <h2 class="text-center text-danger">There is no Products to show!</h2>
                 </div>
-            @endforeach
+            @endif
         </div>
     </div>
 @endsection
