357262beaccf41cee84cc36cd471cce0e738aa7a
Alan2304 <sanchezalan95@gmail.com>
2019-05-10 17:20:24 -0500

    Add statistics to the index

--- resources/views/establishment/index.blade.php
+++ resources/views/establishment/index.blade.php
@@ -5,6 +5,69 @@
 @endsection
 
 @section('content')
+    <div class="row mb-3">
+        <div class="col-md-3">
+            <div class="card">
+                <div class="card-body">
+                    <div class="row">
+                        <div class="col-md-6">
+                            <i class="fas fa-money-check text-warning statistic-icon"></i>
+                        </div>
+                        <div class="col-md-6 info-statistic">
+                            <p>Coupons:</p>
+                            <p>{{$toalCoupons}}</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-3">
+            <div class="card">
+                <div class="card-body">
+                    <div class="row">
+                        <div class="col-md-3">
+                                <i class="fas fa-window-close text-danger statistic-icon"></i>
+                        </div>
+                        <div class="col-md-9 info-statistic">
+                            <p>Products in stock:</p>
+                            <p>{{$productsWithoutSell}}</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-3">
+            <div class="card">
+                <div class="card-body">
+                    <div class="row">
+                        <div class="col-md-4">
+                            <i class="fas fa-money-bill-wave text-success statistic-icon"></i>
+                        </div>
+                        <div class="col-md-8 info-statistic">
+                            <p>$ With coupons:</p>
+                            <p>{{$moneyWithCoupons}}</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-md-3">
+            <div class="card">
+                <div class="card-body">
+                    <div class="row">
+                        <div class="col-md-4">
+                            <i class="fas fa-plus-square text-primary statistic-icon"></i>
+                        </div>
+                        <div class="col-md-8 info-statistic">
+                            <p>Popular Coupon:</p>
+                            <p>{{$couponMostUsed}}</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
     <div class="row">
         @if ($establishments->count() > 0)
             @foreach ($establishments as $establishment)
