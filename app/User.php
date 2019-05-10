cfacfe953f1eaf1e8d78e6649da9c7f55b19a545
Alan2304 <sanchezalan95@gmail.com>
2019-05-10 17:18:38 -0500

    Add functions to show the statistics on the index

--- app/User.php
+++ app/User.php
@@ -6,6 +6,7 @@ use Illuminate\Notifications\Notifiable;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use App\Coupon;
+use App\user_coupon;
 
 class User extends Authenticatable
 {
@@ -49,4 +50,66 @@ class User extends Authenticatable
     public function establishments(){
         return $this->hasMany('App\Establishment');
     }
+
+    public function countCoupons(){
+        $establishments = $this->establishments;
+        $coupons = 0;
+        foreach ($establishments as $establishment) {
+            $coupons += $establishment->coupons->count();
+        }
+        return $coupons;
+    }
+
+    public function moneyCoupons(){
+        $establishments = $this->establishments;
+        $money = 0;
+        foreach ($establishments as $establishment) {
+            $coupons = $establishment->coupons;
+            foreach ($coupons as $coupon) {
+                $couponsUsed = user_coupon::where('coupon_id', $coupon->id)->where('used', '1')->count();
+                $cost = $couponsUsed *( $coupon->product->amount * ($coupon->discount/100) );
+                $money +=  $cost;
+            }
+        }
+        return $money;
+    }
+
+    public function popularCoupon(){
+        $establishments = $this->establishments;
+        $coupon = null;
+        $couponsMostUsed = array();
+        foreach ($establishments as $establishment) {
+            $coupons = $establishment->coupons;
+            foreach ($coupons as $coupon) {
+                $couponsUsed = user_coupon::where('coupon_id', $coupon->id)->where('used', '1')->count();
+                $couponsMostUsed += [$coupon->id => $couponsUsed];
+            }
+        }
+        $prevCount = 0;
+        foreach ($couponsMostUsed as $id => $count) {
+            if (!$coupon) {
+                $coupon = Coupon::where('id', $id)->first();
+                $prevCount = $count;
+            }else{
+                if ($count > $prevCount) {
+                    $coupon = Coupon::where('id', $id)->first();
+                    $prevCount = $count;
+                }
+            }
+        }
+        return $coupon->name;
+    }
+
+    public function productsWithoutSell(){
+        $establishments = $this->establishments;
+        $count = 0;
+        foreach ($establishments as $establishment) {
+            $products = $establishment->products;
+            foreach ($products as $product) {
+                $count += $product->quantity;
+            }
+        }
+        return $count;
+    }
+
 }
